d2c80aa8cd9bd4b086c8c475852109d943f2b097
FelipeFerrete <felipeferretelemes@gmail.com>
2025-09-09 11:25:08 -0300

    Add: Couteudo Page ERROR

--- consulta-certa/src/routes/Erro/index.tsx
+++ consulta-certa/src/routes/Erro/index.tsx
@@ -1,6 +1,12 @@
 function Erro() {
     return(
-        <main id="Erro"></main>
+        <main id="Erro">
+            <h1>
+                <p>
+                    PAGE NOT FOUND - ERROR 404
+                </p>
+            </h1>
+        </main>
     )
 }
 
--- consulta-certa/src/routes/Home/index.tsx
+++ consulta-certa/src/routes/Home/index.tsx
@@ -1,6 +1,90 @@
 function Home() {
     return(
-        <main id="Home"></main>
+        <main>
+            <section className="hero">
+                <h1>Início</h1>
+                <section>
+                    <div className="figure"></div>
+                    <img src="./assets/images/banner_hero.png"
+                        alt="Imagem ilustrativa de atendimento médico atencioso."/>
+                    <div className="figure-inversa"></div>
+                </section>
+                <p>Você não está sozinho, estamos aqui para ajudar.</p>
+            </section>
+            <form action="#" method="get">
+                <label htmlFor="searchBar">
+                    <img src="./assets/images/icon_search_bar.png" alt="Ícone de lupa"/>
+                    <input type="search" name="search" id="searchBar" placeholder="Buscar..."/>
+                </label>
+            </form>
+            <section className="menu">
+                <a href="./pages/guiaHC.html" className="buttonHome">
+                    <img src="./assets/images/variant-icon_guia.png" alt="Ícone de guia"/>
+                    <p>Guia do aplicativo</p>
+                </a>
+                <a href="./pages/guiaTeleconsulta.html" className="buttonHome">
+                    <img src="./assets/images/icon_teleconsulta.png" alt="Ícone de computador com simbolo de saúde"/>
+                    <p>Como será sua consulta</p>
+                </a>
+                <a href="#" className="buttonHome">
+                    <img src="./assets/images/icon_info_medicos.png" alt="Ícone de médico"/>
+                    <p>Profissionais da saúde</p>
+                </a>
+                <a href="#" className="buttonHome">
+                    <img src="./assets/images/icon_info_saúde.png" alt="Ícone de estetoscópio"/>
+                    <p>Cuidados pós consulta</p>
+                </a>
+                <a href="#" className="buttonHome">
+                    <img src="./assets/images/icon_feedback.png" alt="Ícone de pessoa dando feedback"/>
+                    <p>Como foi a teleconsulta</p>
+                </a>
+                <a href="https://www.hc.fm.usp.br/hc/institucional/unidades" target="_blank" className="buttonHome">
+                    <img src="./assets/images/icon_localizacao.png" alt="Ícone de localização"/>
+                    <p>UBS perto de mim</p>
+                </a>
+            </section>
+            <section className="parcerias">
+                <h2>Conheça nossos parceiros</h2>
+                <div className="line"></div>
+                <section className="carrosselParcerias">
+                    <section className="parceiros">
+                        <a href="https://www.iadb.org/pt-br" target="_blank">
+                            <img src="./assets/images/logo_parceiro.png"
+                                alt="Logo do banco interamericano de desenvolvimento"/>
+                        </a>
+                        <a href="https://www.bp.org.br/" target="_blank">
+                            <img src="./assets/images/logo_parceiro1.png"
+                                alt="Logo da beneficiência portuguesa de são paulo"/>
+                        </a>
+                        <a href="https://umane.org.br/" target="_blank">
+                            <img src="./assets/images/logo_parceiro2.png" alt="Logo do grupo umane"/>
+                        </a>
+                        <a href="https://saude.sp.gov.br/" target="_blank">
+                            <img src="./assets/images/logo_parceiro3.png"
+                                alt="Logo da secretaria de saúde de são paulo"/>
+                        </a>
+                    </section>
+                    <section aria-hidden className="parceiros">
+                        <a href="https://www.iadb.org/pt-br" target="_blank">
+                            <img src="./assets/images/logo_parceiro.png"
+                                alt="Logo do banco interamericano de desenvolvimento"/>
+                        </a>
+                        <a href="https://www.bp.org.br/" target="_blank">
+                            <img src="./assets/images/logo_parceiro1.png"
+                                alt="Logo da beneficiência portuguesa de são paulo"/>
+                        </a>
+                        <a href="https://umane.org.br/" target="_blank">
+                            <img src="./assets/images/logo_parceiro2.png" alt="Logo do grupo umane"/>
+                        </a>
+                        <a href="https://saude.sp.gov.br/" target="_blank">
+                            <img src="./assets/images/logo_parceiro3.png"
+                                alt="Logo da secretaria de saúde de são paulo"/>
+                        </a>
+                    </section>
+                </section>
+                <div className="line"></div>
+            </section>
+        </main>
     )
 }
 
