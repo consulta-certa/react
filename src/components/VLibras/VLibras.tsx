85f660ebc907a303a3f7795edc474845e4cdebe9
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-09-21 19:08:08 -0300

    Tentativa de correção para erros do vlibras em chromium based

--- src/components/VLibras/VLibras.tsx
+++ src/components/VLibras/VLibras.tsx
@@ -1,31 +1,25 @@
 // VLibrasWidget.tsx
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 
 function VLibras () {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
-    script.async = true;
+    const script = document.createElement('script')
+    script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js'
+    script.async = true
     script.onload = () => {
-      new (window as any).VLibras.Widget('https://vlibras.gov.br/app');
-    };
-    document.body.appendChild(script);
-  }, []);
+      new (window as any).VLibras.Widget('https://vlibras.gov.br/app')
+    }
+    document.body.appendChild(script)
+  }, [])
 
   return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `
-          <div vw class="enabled">
-            <div vw-access-button class="active"></div>
-            <div vw-plugin-wrapper>
-              <div class="vw-plugin-top-wrapper"></div>
-            </div>
-          </div>
-        `,
-      }}
-    />
-  );
-};
+    <div className='enabled' {...{ vw: '' }}>
+      <div className='active' {...{ 'vw-access-button': '' }}></div>
+      <div {...{ 'vw-plugin-wrapper': '' }}>
+        <div className='vw-plugin-top-wrapper'></div>
+      </div>
+    </div>
+  )
+}
 
-export default VLibras;
+export default VLibras
